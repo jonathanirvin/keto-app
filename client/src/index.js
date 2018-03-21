897d3580f2734cd6351a1f41f0c05064b3d39067
jonathanirvin <irv.jonathan@gmail.com>
2018-03-21 11:31:14 -0500

    Add Basic Redux Setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';  
+import './sass/styles.css';
 import App from './App';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <Header />
+        <App />
+        <Footer />
+      </div>
+    </ConnectedRouter>
+  </Provider>
+
+  , document.getElementById('root'));
 registerServiceWorker();
